e28b14c17a5b382ef7684fce39985ef0fb2c949c
Maximilian Falco Widjaya <z5373431@ad.unsw.edu.au>
2024-02-13 22:58:35 +1100

    Finished stretch task 2

--- frontend/unilectives/src/components/SearchBar.jsx
+++ frontend/unilectives/src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
-import { Box, InputBase } from '@mui/material'
-import React from 'react'
+import { Box, Dialog, InputBase, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { bodyWidth } from './Constants'
 
 const SearchBar = () => {
+  const [open, setOpen] = useState(false);
+  const openDialog = () => {
+    setOpen(true);
+  }
+  const closeDialog = () => {
+    setOpen(false);
+  }
+
   return (
     <Box sx={{
       border: 'solid 0.1rem rgb(156 173 233)',
@@ -18,7 +26,7 @@ const SearchBar = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
       </Box>
-      <InputBase placeholder="Search for a course e.g. COMP1511" sx={{ 
+      <InputBase onClick={openDialog} placeholder="Search for a course e.g. COMP1511" sx={{ 
         flexGrow: 1, 
         fontSize: '0.875em', 
         fontFamily: 'Demibold',
@@ -33,6 +41,35 @@ const SearchBar = () => {
           }
         } 
       }}/>
+      <Dialog open={open} onClose={closeDialog}>
+        <Box sx={{
+          width: '15rem',
+          height: '15rem',
+          overflow: 'hidden'
+        }}>
+          <Box onClick={closeDialog} sx={{
+            cursor: 'pointer',
+            padding: '0.25rem',
+            borderRadius: '50%',
+            width: '1.5rem',
+            height: '1.5rem',
+            position: 'absolute',
+            right: '0.5rem',
+            top: '0.5rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            '&:hover': {
+              backgroundColor: 'lightgray',
+              transition: 'background-color 0.5s'
+            }
+          }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width='18' height='18' fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+          </Box>
+        </Box>
+      </Dialog>
     </Box>
   )
 }
